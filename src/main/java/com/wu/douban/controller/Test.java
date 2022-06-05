1f2473d0cda93b23d8d65f7585c5ce6941ea4518
joker <zhlechevalier@gmail.com>
2022-06-05 16:51:58 +0800

    master commit

--- src/main/java/com/wu/douban/controller/Test.java
+++ src/main/java/com/wu/douban/controller/Test.java
@@ -3,5 +3,7 @@ package com.wu.douban.controller;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello git");
+
+        System.out.println("master commit");
     }
 }
